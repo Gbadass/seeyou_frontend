91680b97f6c4797eefb60aed599e940dd744f033
Gbadass <theboyfakaa@gmail.com>
2024-07-05 02:40:20 +0100

    updated signup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import SignUp from './pages/auth/SignUp';
+import Login from './pages/auth/Login';
 
 function App() {
   return (
     <div className="App">
-<SignUp/>
+<Login/>
     </div>
   );
 }
