6eeb388ff73c87dd93090bfc7d1cc74f8472168b
Gbadass <theboyfakaa@gmail.com>
2024-07-11 00:12:07 +0100

    setting up routes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,20 +1,32 @@
 import {doubleArrow,linkPng} from './../assets'
 import routes from '../routes'
+import { Link } from 'react-router-dom'
 
 
 export default function Sidebar(){
   return(
     <>
     <div className="p-5 sidebar_width border-r h-screen">
-    <div className="flex items-center justify-between">
+      <div className='overflow-scroll'>
+      <div className="flex items-center justify-between">
     <h1 className="font-bold text-2xl">LOGO</h1>
     <img src={doubleArrow} className="w-4 h-4" alt="" />
     </div>
-  
-    <div className='flex gap-3'>
-    <img src={linkPng} className="w-4 h-4" alt="" />
-    <p>Event Types</p>
+
+  <div className='mt-10'>
+  {routes.filter((route)=> route.isSidebar).map((route)=>(
+    <Link>
+    <div className='flex gap-3 mt-8 items-center'>
+<span>{route.icon}</span>
+    <p>{route.name}</p>
     </div>
+    </Link>
+  ))}
+  </div>
+
+
+      </div>
+
     </div>
 
     </>
