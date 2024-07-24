a48938292788e49679905febe6390c64646cc50b
Fakaa Gerald <123073666+Gbadass@users.noreply.github.com>
2024-07-24 07:45:34 +0100

    Update EventTypes.js

--- src/pages/EventTypes.js
+++ src/pages/EventTypes.js
@@ -1,43 +1,51 @@
-import Layout from "../components/Layout"
-import {addPng,LinkPng} from './../assets'
+import Layout from "../components/Layout";
+import {
+  addPng,
+  clockPng,
+  userPng,
+  editPng,
+  linkblackPng,
+  morePng,
+  arrowdownPng
+} from "./../assets";
 import { Box, theme, useTheme } from "@chakra-ui/react";
 import { useState } from "react";
-import NoEvent from './../components/NoEvent'
+import NoEvent from "./../components/NoEvent";
+import Event from "./../components/Event";
+import Calendar from "../components/Calender";
 
-export default function EventTypes(){
+export default function EventTypes() {
   const { colors, fonts } = useTheme(theme);
-  const [noEvent,setNoEvent]=useState(true)
+  const [noEvent, setNoEvent] = useState(true);
 
-  return(
+  return (
     <>
-    <Layout>
-      <div className="">
-      <div className="flex justify-between items-center">
-          <div className="mt-2">
-          <h1 className="text-4xl font-semibold">Event types</h1>
-          <p className="text-sm">create events to share for people to book on your calender</p>
-          </div>
+      <Layout>
+        <div className="">
+          <div className="flex justify-between items-center">
 
-          <div>
-          <button
-              className="top_bar_btn3 flex items-center border rounded-full justify-center gap-2 py-3"
-              style={{ borderColor: colors.primary_purp[100] }}
-            >
-              <img src={addPng} alt="" className="w-4 h-4" />
-              <p
-                className="text-base  font-medium"
-                style={{ color: colors.primary_purp[100] }}
+
+            <div>
+              <button
+                className="top_bar_btn3 flex items-center border rounded-full justify-center gap-2 py-3"
+                style={{ borderColor: colors.primary2[100] }}
               >
-                New Event Type
-              </p>
-            </button>
+                <img src={addPng} alt="" className="w-4 h-4" />
+                <p
+                  className="text-base  font-medium"
+                  style={{ color: colors.primary2[100] }}
+                >
+                  Create
+                </p>
+                <img src={arrowdownPng} alt="" className="w-5 h-5" />
+              </button>
+            </div>
           </div>
+
+
+          {noEvent?<Calendar/>:""}
         </div>
-
-{noEvent?<NoEvent/>:""}
-
-      </div> 
-    </Layout>
+      </Layout>
     </>
-  )
+  );
 }
